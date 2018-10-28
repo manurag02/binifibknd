9ea6596974f44cbfd5c42f2697a27a4f0a97ed0a
manurag02 <manurag02@gmail.com>
2018-10-28 13:19:00 +0530

    first

---
+++ index.js
@@ -0,0 +1,35 @@
+// calling the module
+var express = require("express");
+
+//creating an instance
+var app = express();
+
+// we have to include events module - core nodejs
+var events = require("events");
+
+// you have create an instance of event emitter
+var eventEmitter = new events.EventEmitter();
+
+// a basic route
+app.get("/signup", function(req, res) {
+  // create a user
+  //save it
+
+  //send him a welcome email using event
+
+  var user = { name: "Aditya", email: "aditya@edwisor.com" };
+
+  // this is how event is emitted.
+  setTimeout(() => {
+    eventEmitter.emit("welcomeEmail", user);
+  }, 2000);
+
+  // simple response
+  console.log("sending response");
+  res.send("Hello World!");
+});
+
+//listening on a port
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
